3a1ace19b36342e83ba031d1b4206feb0f6de8b4
@Margarita52 <muzovatkinamargarita@gmail.com>
2025-11-16 22:42:40 +0300

    AdminPage.tsx

---
+++ src/pages/admin/AdminPage.tsx
@@ -0,0 +1,9 @@
+import { BackButton } from "../../components/BackButton";
+export default function AdminHome() {
+  return (
+    <>
+      <h1>Панель администратора</h1>
+      <BackButton />
+    </>
+  );
+}
